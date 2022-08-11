2048fe8702ca8d1ad5aea45b9b9bbb0ec65e34d3
Joao Marcos Teixeira Ribeiro <jima.teixeira10@gmail.com>
2022-08-10 21:20:30 -0300

    ajustando detalhes, animando dado e gerando frase automaticamente

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-// console.log('oi')
+const generate = document.querySelector('.btn')
+const id = document.querySelector('.id')
+const quote = document.querySelector('.quote')
 
-fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(json => console.log(json.slip.advice))
+window.onload = () => {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(json => {
+            const idNumber = json.slip.id
+            const advice = json.slip.advice
+            quote.textContent = advice
+            id.textContent = idNumber
+        })
+}
+generate.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(json => {
+            const idNumber = json.slip.id
+            const advice = json.slip.advice
+            quote.textContent = advice
+            id.textContent = idNumber
+        })
+    })
